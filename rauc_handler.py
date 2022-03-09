6f747c9fa7a94319c5ac0dd2dad0cecce18c6d7c
patrikSohlman <patrik.sohlman@hotmail.se>
2022-03-09 07:53:55 +0100

    Gdbus

--- rauc_handler.py
+++ rauc_handler.py
@@ -1,35 +1,45 @@
 import signal
 import os
-from pydbus import SystemBus
+from gi.repository import GLib
+from gi.repository import Gio
 
 
 class RaucHandler():
     def __init__(self):
-        self.bus = SystemBus()
-        # the service we request
-        self.service_name = 'de.pengutronix.rauc'
-        # The interface we request of the service
-        self.interface_name = 'de.pengutronix.rauc.Installer'
-        # The object we request
-        self.object_path = '/'
-        remote_object = self.bus.get(self.service_name, '/')
 
-        # help(remote_object)
-        self.iface = remote_object['de.pengutronix.rauc.Installer']
-        #slot_status = iface.GetSlotStatus()
-        # print(slot_status)
+
+        self.bus = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
+        self.dbus_proxy = Gio.DBusProxy.new_sync(bus,
+                                            Gio.DBusProxyFlags.NONE,
+                                            None,
+                                            'de.pengutronix.rauc',
+                                            '/',
+                                            'de.pengutronix.rauc.Installer',
+                                            None)
+        
 
     def get_slot_status(self):
-        slot_status = self.iface.GetSlotStatus()
+        slot_status = self.dbus_proxy.call_sync('GetSlotStatus',
+                                                GLib.Variant('(s)', (state,partition)),
+                                        Gio.DBusCallFlags.NO_AUTO_START, 500, None)
         return slot_status
 
     def mark_partition(self, state, partition):
-        return self.iface.Mark(state, partition)
+        mark_result = self.dbus_proxy.call_sync('Mark',
+                                                GLib.Variant('(s)', (state,partition)),
+                                        Gio.DBusCallFlags.NO_AUTO_START, 500, None)
+        return mark_result
 
     def install(self, path):
-        self.iface.Install(path)
-        self.iface.on('Completed', lambda nb: self.reboot_after_install(nb))
+        try:
+            self.dbus_proxy.call_sync('Install',
+                                        GLib.Variant('(s)', (path,)),
+                                        Gio.DBusCallFlags.NO_AUTO_START, 500, None)
+            self.reboot_after_install()
+        except Exception as e:
+            print(f"Error in rauc_handler.install: {e}")
+        
     
     def reboot_after_install(self, nb):
         print("Received completed from RAUC interface")
-        os.system("sudo /sbin/reboot") 
+        os.system("/sbin/reboot") 
