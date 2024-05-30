bfcaca24b9fbaed83805b8d4e2792ba097c39002
wmmihaa <wmmihaa@hotmail.com>
2024-05-30 10:10:36 +0200

    ...

--- src/utils.py
+++ src/utils.py
@@ -12,28 +12,31 @@ def getHwAddr(ifname):
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
     return ':'.join('%02x' % b for b in info[18:24])
 
-def check_version(msb_dir):
+def check_version(msb_dir, log):
     try:
+        if (log == None):
+            log = print
+
         # Check if directory exists
         if(msb_dir == ""):
             msb_dir = f"{os.environ['HOME']}/msb-py"
         
-        if os.path.isdir(msb_dir) == False:
+        if( os.path.isdir(msb_dir) == False):
             return False
 
-        print("")
-        print("*********************************************")
-        print (f"current dir: {os.path.dirname(os.path.abspath(__file__))}")
+        log("")
+        log("*********************************************")
+        log (f"current dir: {os.path.dirname(os.path.abspath(__file__))}")
         currentDirectory = os.path.dirname(os.path.abspath(__file__));
         preferedVersion = "latest"
         currentVersion = ""
         msb_settings_path = f"{msb_dir}/settings.json"
-        print(f"msb_settings_path: {msb_settings_path}")
+        log(f"msb_settings_path: {msb_settings_path}")
         if os.path.exists(msb_settings_path):
             with open(msb_settings_path) as f:
                 settings = json.load(f)
                 preferedVersion = settings["coreVersion"]
-                print(f"preferedVersion: {preferedVersion}")
+                log(f"preferedVersion: {preferedVersion}")
         else:
             print(f"{preferedVersion} does not exist")
             return False
@@ -53,10 +56,10 @@ def check_version(msb_dir):
             encoding = response.info().get_content_charset('utf-8')
             latest_release = json.loads(data.decode(encoding))
             preferedVersion = latest_release["tag_name"]
-            print(f"preferedVersion: {preferedVersion}")
+            log(f"preferedVersion: {preferedVersion}")
         
         if preferedVersion != currentVersion:
-            print(f"Updating from {currentVersion} to {preferedVersion}")
+            log(f"Updating from {currentVersion} to {preferedVersion}")
             gitUri = f"https://api.github.com/repos/axians/microservicebus-py/releases"
             response = urllib.request.urlopen(gitUri)
             data = response.read()
@@ -73,43 +76,43 @@ def check_version(msb_dir):
                 install_dir = f"{currentDirectory}/../install"
                 if os.path.exists(f"{currentDirectory}/../install"):
                     shutil.rmtree(f"{currentDirectory}/../install")
-                    print("Removed install directory")
+                    log("Removed install directory")
 
                 tar_file = tarfile.open(fileobj=response.raw, mode="r|gz")
                 tar_file.extractall(path=install_dir)
                 top_directory = os.listdir(install_dir)[0]
-                print(f"top_directory: {top_directory}")
+                log(f"top_directory: {top_directory}")
                 
                 src_directory = f"{install_dir}/{top_directory}/src"
                 dest_directory = f"{currentDirectory}/../src_new/"
-                print(f"src_directory: {src_directory}")
-                print(f"dest_directory: {dest_directory}")
+                log(f"src_directory: {src_directory}")
+                log(f"dest_directory: {dest_directory}")
 
                 if os.path.exists(f"{currentDirectory}/../src_new"):
                     shutil.rmtree(f"{currentDirectory}/../src_new")
-                    print("Removed src_new directory")
+                    log("Removed src_new directory")
 
                 shutil.copytree(src_directory, dest_directory)
                 shutil.rmtree(src_directory)
                 if os.path.exists(f"{currentDirectory}/../src_old"):
                     shutil.rmtree(f"{currentDirectory}/../src_old")
-                    print("Removed src_old directory")
+                    log("Removed src_old directory")
                 
                 shutil.copytree(f"{currentDirectory}", f"{currentDirectory}/../src_old")
                 #os.rename(f"{currentDirectory}/src", f"{currentDirectory}/../src_old")
                 for filename in os.listdir(currentDirectory):
-                    print(f"\tRemoving {filename}")
+                    log(f"\tRemoving {filename}")
                     os.remove(f"{currentDirectory}/{filename}")
 
                 for filename in os.listdir(f"{currentDirectory}/../src_new"):
-                    print(f"\tCopying {filename}")
+                    log(f"\tCopying {filename}")
                     shutil.copyfile(f"{currentDirectory}/../src_new/{filename}", f"{currentDirectory}/{filename}")
                 #os.rename(f"{currentDirectory}/../src_new", f"{currentDirectory}")
-                print("Successfully updated")
+                log("Successfully updated")
                 return True
     
     except Exception as e:
-        print(f"Failed to update version: {e}")
+        log(f"Failed to update version: {e}")
         return False
 
 def red(str):
